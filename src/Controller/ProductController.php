dc0b345be0702f664da85f6dc7499c0f47b5a85b
Axek <axel.phanor64@gmail.com>
2023-05-17 18:39:17 +0200

    update

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/product', name: 'app_product')]
     public function index(): Response
     {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        //dd($products);
+        
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
 }
