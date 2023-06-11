498414244695f8f5fc8274597dbe8d64363f75f4
Axek <axel.phanor64@gmail.com>
2023-06-12 00:44:07 +0200

    ajout adresse

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -7,9 +7,19 @@ use App\Form\AddressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class AccountAddressController extends AbstractController
 {
+    private $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte/adresse', name: 'app_account_address')]
     public function index(): Response
     {
@@ -17,11 +27,55 @@ class AccountAddressController extends AbstractController
     }
 
     #[Route('/compte/ajouter-adresse', name: 'app_account_address_add')]
-    public function add()
+    public function add(Request $request)
     {
         $address = new Address();
         $form = $this->createForm(AddressType::class, $address);
-        return $this->render('account/addressAdd.html.twig', [
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+            $address = $form->getData();
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+            
+            return $this->redirectToRoute('app_account_address');
+        }
+
+        return $this->render('account/addressForm.html.twig', [
+            'form' => $form->createView(),
+            'address' => $address,
+        ]);
+    }
+
+    #[Route('/compte/supprimer-adresse/{id}', name: 'app_account_address_delete')]
+    public function delete(Request $request, $id)
+    {
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+        if($address && $address->getUser() == $this->getUser()) {
+            $this->entityManager->remove($address);
+            $this->entityManager->flush();
+        }
+        return $this->redirectToRoute('app_account_address');
+    }
+
+    #[Route('/compte/modifier-adresse/{id}', name: 'app_account_address_edit')]
+    public function edit(Request $request, $id)
+    {
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+        
+        if(!$address || $address->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('app_account_address');
+        }
+
+        $form = $this->createForm(AddressType::class, $address);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_account_address');
+        }
+
+        return $this->render('account/addressForm.html.twig', [
             'form' => $form->createView(),
         ]);
     }
--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -7,23 +7,75 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 
 class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('address')
-            ->add('postal')
-            ->add('city')
-            ->add('country')
-            ->add('phone')
+            ->add('name', TextType::class, [
+                'label' => 'Quel nom souhaitez-vous donner à votre adresse ?',
+                'attr' => [
+                    'placeholder' => 'Nommez votre adresse',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Saisir votre prénom',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom de famille',
+                'attr' => [
+                    'placeholder' => 'Saisir votre nom de famille',
+                ],
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Société',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => '(facultatif)',
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Saisir votre adresse',
+                'attr' => [
+                    'placeholder' => '18 Avenue Louise Darracq',
+                ],
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'Saisir code postal',
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Saisir votre ville',
+                ],
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'Saisir votre pays',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Numéro de téléphone',
+                'attr' => [
+                    'placeholder' => 'Saisir numéro de téléphone',
+                ],
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Ajouter adresse',
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn btn-dark btn-block',
+                ]
             ])
         ;
     }
