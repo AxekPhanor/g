b70345cdb71d6d1a1edd728083baa035c3a07a6e
Axek <axel.phanor64@gmail.com>
2023-06-12 11:18:43 +0200

    add entity Order, OrderDetails and Carrier

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\Carrier;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -42,6 +43,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
-        yield MenuItem::linkToCrud('Produits', 'fa fa-tag', Product::class);
+        yield MenuItem::linkToCrud('Products', 'fa fa-tag', Product::class);
+        yield MenuItem::linkToCrud('Carriers', 'fa fa-truck', Carrier::class);
     }
 }
